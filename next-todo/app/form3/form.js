24192ddba043427b2dd5c877dc85437183792dd2
zione <346691836@qq.com>
2025-02-26 15:53:14 +0800

    feat: sever action

---
+++ next-todo/app/form3/form.js
@@ -0,0 +1,32 @@
+'use client'
+ 
+import { useFormStatus } from 'react-dom'
+import { createToDo } from '@/actions/form3';
+import React from 'react';
+
+const initialState = {
+  message: '',
+}
+ 
+function SubmitButton() {
+  const { pending } = useFormStatus()
+  return (
+    <button type="submit" aria-disabled={pending}>
+      {pending ? 'Adding' : 'Add'}
+    </button>
+  )
+}
+
+export default function AddToDoForm() {
+  const [state, formAction] = React.useActionState(createToDo, initialState)
+ 
+  return (
+    <form action={formAction}>
+      <input type="text" name="todo" />
+      <SubmitButton />
+      <p aria-live="polite" >
+        {state?.message}
+      </p>
+    </form>
+  )
+}
