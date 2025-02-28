e41f03ae1d404516770284f55d131b0205aa7c9d
zione <346691836@qq.com>
2025-02-28 17:31:54 +0800

    chore: font and css

--- next-todo/app/layout.js
+++ next-todo/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { geistMono, geistSans } from "@/styles/fonts";
+import "@/styles/globals.css"
 
 export const metadata = {
   title: "Create Next App",
